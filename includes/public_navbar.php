5abb02fb0b7121c812813ec6eb6b62f0d436535e
Yomalj <yomaljayasooriya698@gmail.com>
2026-07-27 13:08:52 +0530

    Create shared layout and public pages

---
+++ includes/public_navbar.php
@@ -0,0 +1,16 @@
+<nav class="public-navbar">
+    <div class="navbar-container">
+        <a class="navbar-brand" href="/restaurant-website/index.php">
+            Restaurant
+        </a>
+
+        <div class="navbar-links">
+            <a href="/restaurant-website/index.php">Home</a>
+            <a href="/restaurant-website/menu.php">Menu</a>
+            <a href="/restaurant-website/about.php">About</a>
+            <a href="/restaurant-website/contact.php">Contact</a>
+            <a href="/restaurant-website/reservation.php">Reservation</a>
+            <a href="/restaurant-website/user/login.php">Login</a>
+        </div>
+    </div>
+</nav>
